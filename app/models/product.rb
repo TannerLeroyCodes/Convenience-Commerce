77f2d796033b50f0573045e71293316077a16303
tannerleroy <tanner@tleroy.com>
2022-06-20 19:34:17 -0400

    added user controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :price, numericality: {greater_than: 0}, presence: true
   validates :stock, numericality: {greater_than_or_equal_to: 0}, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :orders 
+    has_many :order_items, through: :orders
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true 
+
 end
