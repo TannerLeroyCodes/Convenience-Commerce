fbb7b547d3c12794edba31971d0728d9ad5452f1
tannerleroy <tanner@tleroy.com>
2022-06-20 10:35:57 -0400

    deleting extra react content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def hello_world
         session[:count] = (session[:count] || 0) + 1
@@ -13,12 +13,12 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_resp
 
   private 
 
-  def render_unprocessable_entity_response(exception)
-    render json: {errors: exception.record.errors}, status: :unprocessable_entity
-  end 
+#   def render_unprocessable_entity_response(exception)
+#     render json: {errors: exception.record.errors}, status: :unprocessable_entity
+#   end 
     
-  def render_not_found_response(exception)
-    render json: {error: "#{exception.model} not found"}, status: :not_found
-  end
+#   def render_not_found_response(exception)
+#     render json: {error: "#{exception.model} not found"}, status: :not_found
+#   end
 
 end
