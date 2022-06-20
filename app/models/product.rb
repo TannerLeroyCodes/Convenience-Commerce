14b71301563e19a86cc8292345b7934a7d92ad6e
tannerleroy <tanner@tleroy.com>
2022-06-20 19:04:56 -0400

    bug fix in validates product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
 
-  validates price:, numericality: {greater_than: 0}, presence: true
-  validates stock:, numericality: {greater_than_or_equal_to: 0}, presence: true
-  validates description:, length: 10..50, presence: true
+  validates :price, numericality: {greater_than: 0}, presence: true
+  validates :stock, numericality: {greater_than_or_equal_to: 0}, presence: true
+  validates :description, length: {in: 10..50}, presence: true
 
-end
+endgit 
