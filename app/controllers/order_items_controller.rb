a6335ed766b0cee9392c9ef26100c97622522d91
tannerleroy <tanner@tleroy.com>
2022-06-21 11:17:12 -0400

    added object_items controller info

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,36 @@
 class OrderItemsController < ApplicationController
+
+def index 
+    render json: OrderItem.all 
+end 
+
+def show
+    order_item = OrderItem.find(params[:id])
+    render json: order_item
+end 
+
+def create 
+    order_item = OrderItem.create!(order_item_params)
+    render json: order_item, status: :created
+end 
+
+def update 
+    order_item = OrderItem.find(params[:id])
+    order_item.update!(order_item_params)
+    render json: order_item, status: :created
+end 
+
+def destroy
+    order_item = OrderItem.find(params[:id])
+    order_item.destroy
+    head :no_content
+end 
+
+private 
+
+def order_items_params
+    params.permit(:product_id, :order_id, :quantity)
+end 
+
+
 end
