3337979c0ad4aef5e7ba01a8b3eb1b820f837c0d
tannerleroy <tanner@tleroy.com>
2022-06-21 14:24:00 -0400

    added user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 puts "Seeding begins..."
 
+puts "Seeding Users..."
 
-User.create(first_name: "Tanner", last_name: "Leroy", email: "tanner@tleroy.com", admin: true, )
+User.create(first_name: "Tanner", last_name: "Leroy", email: "tanner@tleroy.com", admin: true, password: "password" )
+User.create(first_name: "Bella", last_name: "Jo", email: "bella@tleroy.com", admin: true, password: "password" )
