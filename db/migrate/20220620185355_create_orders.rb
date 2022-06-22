e6cb72e19051bb277d00a9b43d02e463fe54cbb9
imj22 <98693440+imj22@users.noreply.github.com>
2022-06-22 14:38:59 +0200

    test

--- db/migrate/20220620185355_create_orders.rb
+++ db/migrate/20220620185355_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
-      t.boolean :ordered
+      t.boolean :ordered, default: false
 
       t.timestamps
     end
