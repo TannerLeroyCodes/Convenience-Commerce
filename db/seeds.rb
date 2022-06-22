d68694bf4ac5ba1e907f7e1b7e5fb07972148bf1
imj22 <98693440+imj22@users.noreply.github.com>
2022-06-22 10:51:10 +0200

    first commit, messing around with seed data and faker gem. will be back to work soon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
+require 'faker'
+
 puts "Seeding begins..."
 
 puts "Seeding Users..."
 
 User.create(first_name: "Tanner", last_name: "Leroy", email: "tanner@tleroy.com", admin: true, password: "password" )
 User.create(first_name: "Bella", last_name: "Jo", email: "bella@tleroy.com", admin: true, password: "password" )
+User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.unique.email, admin: false, password: "user" )
+
+
+
+Category.create(type: "Electronics")
+Category.create(type: "Furniture")
+Category.create(type: "Games")
+
+Product.create(category: "Furniture", description: Faker::House.furniture, stock: rand(0..100), price: rand(0.00..1000.00), favorite: false, image_url: )
+
