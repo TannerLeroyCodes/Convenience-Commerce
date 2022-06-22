f9118d36329381c22aa8eddf66d4574492b956f2
tannerleroy <tanner@tleroy.com>
2022-06-22 06:56:41 -0400

    created components for front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,13 @@ puts "Seeding Users..."
 
 User.create(first_name: "Tanner", last_name: "Leroy", email: "tanner@tleroy.com", admin: true, password: "password" )
 User.create(first_name: "Bella", last_name: "Jo", email: "bella@tleroy.com", admin: true, password: "password" )
+
+
+puts "Seeding Categories.."
+
+Category.create(type: "Elecontrics")
+
+
+puts "Seeding Products..."
+
+
