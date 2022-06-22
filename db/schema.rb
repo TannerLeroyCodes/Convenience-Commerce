8c995f21037910aa0b359691002e914d96c11d14
tannerleroy <tanner@tleroy.com>
2022-06-22 08:46:30 -0400

    added name to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_185602) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_124439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_185602) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
