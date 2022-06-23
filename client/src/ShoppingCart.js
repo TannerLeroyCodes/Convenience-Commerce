837df888b357db4a8115beb66f25c9ac072ebd50
tannerleroy <tanner@tleroy.com>
2022-06-23 17:27:39 -0400

    added navigation to products from shoppoing cart

--- client/src/ShoppingCart.js
+++ client/src/ShoppingCart.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 
 function ShoppingCart() {
 
 const currentOrder = useSelector((state) => state.currentOrder.value)
+const history = useNavigate();
   
 
 function handleSubmit(e){
@@ -18,12 +20,17 @@ function handleSubmit(e){
     });
 }
 
+function handleProducts(){
+history('/products')
+}
+
 
   return (
     <>
  {currentOrder.id ? <> 
     <h1>ShoppingCart</h1>
     
+    <button onClick={handleProducts}>Add more Items to Cart</button>
     <div>Order ID:{currentOrder.id}</div>
     <div>Order Submitted: {currentOrder.ordered ? "Order has been submitted" : "Order has not been submitted"}</div>
     <button onClick={handleSubmit}>Submit your order</button>
