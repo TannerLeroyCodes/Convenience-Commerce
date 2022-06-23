e5f7d79dece1986e13084291b4b6b3c8474f9d7a
imj22 <98693440+imj22@users.noreply.github.com>
2022-06-23 11:52:53 +0200

    minor auth debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,25 +5,26 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # ensuring users must be authorized (authenticated to access any portion of the code)
-  before_action :is_logged_in? 
-  # before_action :is_authorized?
+  # before_action :is_logged_in? 
+  before_action :is_authorized?
 
   def current_user
     User.find_by(id: session[:user_id])
   end
 
-  def is_logged_in? 
-    # byebug
-    render json: {errors: ["Must log-in or create an account to access Convenient Commerce"]}, status: :unauthorized unless current_user
-  end
+  # def is_logged_in? 
+  #   # byebug
+  #   render json: {errors: ["Must log-in or create an account to access Convenient Commerce"]}, status: :unauthorized unless current_user
+  # end
 
   def is_admin?
     render json: {errors: ["Not authorized"]}, status: :unauthorized unless current_user.admin
   end
 
   def is_authorized? 
+    # byebug
     # @current_user = User.find_by(id: session[:user_id])
-    render json: {errors: ["Not authorized"]}, status: :unauthorized unless current_user
+    render json: {errors: ["Not authorized", "Must log-in or create an account to access Convenient Commerce"]}, status: :unauthorized unless current_user
   end
 
   private 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
-    skip_before_action :is_logged_in?, only: :create
-    # skip_before_action :is_authorized?, only: :create
+    # skip_before_action :is_logged_in?, only: :login
+    skip_before_action :is_authorized?, only: :login
 
     def login
         
         user = User.find_by(email: params[:email])
         if  user&.authenticate(params[:password])
-          session[:user_id] ||= user.id
-          render json: user, serializer: UserSerializer, status: :ok
+          session[:user_id] = user.id
+          render json: user, status: :ok #, serializer: UserSerializer
         else
           render json: { errors: ["Invalid username or password"] }, status: :unauthorized
         end
