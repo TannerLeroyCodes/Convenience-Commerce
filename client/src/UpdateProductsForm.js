327c00213af475780605ddfc3decdc7bc5edeeb7
imj22 <98693440+imj22@users.noreply.github.com>
2022-06-24 10:40:33 +0200

    state is functional/dynamic for update form, will now create the submit handler and patch request

--- client/src/UpdateProductsForm.js
+++ client/src/UpdateProductsForm.js
@@ -1,38 +1,38 @@
 import React, {useState} from 'react'
 
 function UpdateProductsForm({product, product: {category, description, favorite, id, image_url, name, stock, price}}) {
-  const [categoryValue, setCategoryValue] = useState(category)
-  const [descriptionValue, setDescriptionValue] = useState(description)
-  const [favoriteValue, setFavoriteValue] = useState(favorite)
-  const [imageUrlValue, setImageUrlValue] = useState(image_url)
-  const [nameValue, setNameValue] = useState(name)
-  const [stockValue, setStockValue] = useState(stock)
-  const [priceValue, setPriceValue] = useState(price)
+  const [categoryValue, setCategoryValue] = useState("")
+  const [descriptionValue, setDescriptionValue] = useState("")
+  const [favoriteValue, setFavoriteValue] = useState("")
+  const [imageUrlValue, setImageUrlValue] = useState("")
+  const [nameValue, setNameValue] = useState("")
+  const [stockValue, setStockValue] = useState("")
+  const [priceValue, setPriceValue] = useState("")
 
     return (
     <div>UpdateProductsForm
         <form>
             <label>Name
-                <input type='text'></input>
+                <input type='text' value={nameValue}  onChange={e => setNameValue(e.target.value)}></input>
             </label>
             <label>Description
-                <input type='text'></input>
+                <input type='text' value={descriptionValue} onChange={e => setDescriptionValue(e.target.value)}></input>
             </label>
             <label>Stock
-                <input type='text'></input>
+                <input type='text' value={stockValue} onChange={e => setStockValue(e.target.value)}></input>
             </label>
             <label>Price
-                <input type='text'></input>
+                <input type='text' value={priceValue} onChange={e => setPriceValue(e.target.value)}></input>
             </label>
             <label>Image Url
-                <input type='text'></input>
+                <input type='text' value={imageUrlValue} onChange={e => setImageUrlValue(e.target.value)}></input>
             </label>
             <label>Category Name
-                <input type='text'></input>
+                <input type='text' value={categoryValue} onChange={e => setCategoryValue(e.target.value)}></input>
             </label>
-            <select>
-                <option value="false">favorite (true)</option>
-                <option value="true">un-favorite (false)</option>
+            <select onChange={e => setFavoriteValue(e.target.value)}>
+                <option value="true">favorite (true)</option>
+                <option value="false">un-favorite (false)</option>
             </select>
 
             <input type='submit'></input>
