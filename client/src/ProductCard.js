b0ac1ecb6fb8c11291047fad0d8ce70c350b766d
imj22 <98693440+imj22@users.noreply.github.com>
2022-06-24 12:35:28 +0200

    minor changes, will debug patch request to work synchronously with update form submit

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -32,7 +32,7 @@ function ProductCard({product, updateProduct, product: {category, description, f
     <div>
       <img src={image_url} alt={name} className="product-card"></img>
       <h2>{name}</h2>
-      <h3>{description}</h3>
+      <h3>Description: {description}</h3>
       <h3>Category: {category.name}</h3>
       <h3>Price: ${price}</h3>
       <h3>Amount in stock: {stock}</h3>
